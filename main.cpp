2bd0e4d732e398f53292a2fe2fc4ef35c158cec0
Satoshi Shimada <mylinux1204@gmail.com>
2019-07-24 16:01:12 +0900

    modify sample code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <stdlib.h>
-
-#include <unistd.h>
-
 #include "RS405CB.h"
 
 int main(int argc, char *argv[])
@@ -13,19 +10,13 @@ int main(int argc, char *argv[])
 
 	RS405CB servo("/dev/ttyUSB0", 115200);
 
-	double voltage = servo.getVoltage(id);
+	const double voltage = servo.getVoltage(id);
 	std::cout << "voltage: " << voltage << std::endl;
-	int temperature = servo.getTemperature(id);
+	const int temperature = servo.getTemperature(id);
 	std::cout << "temperature: " << temperature << std::endl;
 	std::cout << "angle: " << servo.getAngle(id) << std::endl;
 	servo.setTorque(id, true); // torque on
-
-	//servo.setAngle(id, 0.0);
-	for(int i = -14; i < 15; i++) {
-		servo.setAngle(id, i * 0.1);
-		sleep(1);
-	}
-	std::cout << "angle: " << servo.getAngle(id) << std::endl;
+	servo.setAngle(id, 0.0);
 
 	return 0;
 }
